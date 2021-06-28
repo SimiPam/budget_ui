7499092066b66ca5fc340233df24098b6307233a
SimiPam <simipamdachomo@gmail.com>
2021-06-28 22:05:14 +0100

    personal expenses ui

--- lib/widgets/cardSection.dart
+++ lib/widgets/cardSection.dart
@@ -18,45 +18,47 @@ class CardSection extends StatelessWidget {
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             )),
         Expanded(
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 2,
-                itemBuilder: (context, i) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                    decoration: BoxDecoration(
-                        color: primaryColor,
-                        boxShadow: customShadow,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                          top: 100,
-                          bottom: -200,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                boxShadow: customShadow,
-                                shape: BoxShape.circle,
-                                color: Colors.white38),
-                          ),
-                        ),
-                        Positioned.fill(
-                          left: -300,
-                          top: -100,
-                          bottom: -100,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                boxShadow: customShadow,
-                                shape: BoxShape.circle,
-                                color: Colors.white24),
-                          ),
-                        ),
-                        CardDetails()
-                      ],
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 2,
+            itemBuilder: (context, i) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                decoration: BoxDecoration(
+                    color: primaryColor,
+                    boxShadow: customShadow,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Stack(
+                  children: [
+                    Positioned.fill(
+                      top: 100,
+                      bottom: -200,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white38),
+                      ),
                     ),
-                  );
-                }))
+                    Positioned.fill(
+                      left: -300,
+                      top: -100,
+                      bottom: -100,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white24),
+                      ),
+                    ),
+                    CardDetails()
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
